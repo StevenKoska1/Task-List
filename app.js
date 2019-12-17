8025e625426771b5bafbaf9ab2ec9cfacccd2399
Opilane TTHK <opilane@tthk.ee>
2019-12-17 11:35:14 +0200

    2nd commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const taskInput = document.querySelector("#task");
+const filterInput = document.querySelector("#filter");
+const clearBtn = document.querySelector(".clear-tasks");
+
+loadEventListener();
+
+function loadEventListener() {
+  //Add a task event
+  form.addEventListener("submit", addTask);
+  //remove task
+  taskList.addEventListener("click", removeTask);
+  //clear all tasks
+  clearBtn.addEventListener("click", clearTasks);
+  //filter tasks
+  filterInput.addEventListener('keyup', filterTasks);
+}
+
+function addTask(event) {
+    //check for empt input
+  if (taskInput.value === "") {
+    alert("Enter a task");
+  }
+  //create an li element to add to the ul
+  const li = document.createElement("li");
+  //add a class name to the li element
+  li.className = "collection-item";
+  li.appendChild(document.createTextNode(taskInput.value));
+  //create a new anchor element
+  const link = document.createElement("a");
+  //add a class to the 'a' element
+  link.className = "delete-item secondary-content";
+  link.innerHTML = "X";
+  li.appendChild(link);
+
+  taskList.appendChild(li);
+
+  event.preventDefault();
+}
+
+function removeTask(event) {
+  if (event.target.classList.contains("delete-item")) {
+    event.target.parentElement.remove();
+  }
+}
+
+function clearTasks() {
+  if (confirm("Clear the task list?")) {
+    while (taskList.firstChild) {
+      taskList.removeChild(taskList.firstChild);
+    }
+  }
+
+  function filterTasks(event){
+      const userFilter = event.target.value.toLowerCase();
+
+      document.querySelectorAll(".collection-item").forEach(function(task){
+          const item = task.firstChild.textContent;
+          if(item.toLocaleLowerCase().indexOf(userFilter) != -1){
+              task.style.display = 'block';
+          }else{
+              task.style.display = 'none';
+          }
+      })
+      console.log(userFilter);
+  }
+}
